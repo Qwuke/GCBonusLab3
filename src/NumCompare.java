f579a1bcd821963f3667a386c1e062bfce146def
Tristan Mortimer <tristan.e.mortimer@gmail.com>
2017-10-24 18:06:22 -0400

    Created compare method return template

--- src/NumCompare.java
+++ src/NumCompare.java
@@ -1,4 +1,13 @@
 
 public class NumCompare {
-
+	public static boolean compareNum(int targetNum, double guess){
+		double targetDouble = targetNum;
+		if(targetDouble == guess) {
+			System.out.println("You got it right");
+		}
+		
+		
+		
+		return true;
+	}
 }
