9e1dea1983d76e204f98b167aba3f91fe7348a5d
Tristan Mortimer <tristan.e.mortimer@gmail.com>
2017-10-25 16:38:05 -0400

    Added conditional path for determining how close someone is to the number

--- src/NumCompare.java
+++ src/NumCompare.java
@@ -4,10 +4,21 @@ public class NumCompare {
 		double targetDouble = targetNum;
 		if(targetDouble == guess) {
 			System.out.println("You got it right");
+			return true;
+		} else if(targetDouble > guess) {
+			if(10<=(targetDouble - guess)) {
+				System.out.println("Too high!");
+			} else {
+				System.out.println("Way too high!");
+			}
+			return false;
+		} else {
+			if(10<=(guess - targetDouble)) {
+				System.out.println("Too low!");
+			} else {
+				System.out.println("Way too low!");
+			}
+			return false;
 		}
-		
-		
-		
-		return true;
 	}
 }
